36bc30cb108ed70e0105bbded3dfe3964be371f3
Erik Cederblad <erik.cederblad@meds.se>
2023-01-23 22:17:57 +0100

    some formatting examples

---
+++ WebApplication1/DotnetFormat/Formatting.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApplication1.DotnetFormat
+{
+    public class Formatting
+    {
+        public bool PreferBraces(bool success)
+        {
+            if (success)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void NewLineBeforeMembersInObjectInitializers()
+        {
+            var z = new
+            {
+                A = 3,
+                B = 4
+            };
+
+        }
+
+        public void VarForBuiltInTypes()
+        {
+            string s = "hejsvej";
+        }
+    }
+}
