7652883a83acbc1dcc9631127ea10512b46f0a0c
rebecka-meds <111272999+rebecka-meds@users.noreply.github.com>
2023-01-23 16:33:54 +0100

    Change let to var

--- WebApplication1/ClientApp/web/scripts/modules/testModule.ts
+++ WebApplication1/ClientApp/web/scripts/modules/testModule.ts
@@ -1,11 +1,12 @@
-﻿let updateTestModule = () => {
-  let testModuleElement = document.querySelector(".test-module") as HTMLElement;
+﻿var updateTestModule = () => {
+  var testModuleElement = document.querySelector(".test-module") as HTMLElement;
   if (!testModuleElement) {
     return;
   }
+
   setTimeout(() => {
     testModuleElement.innerHTML += "<br>And this text is added in the client.";
   }, 500);
-}
+};
 
 updateTestModule();
